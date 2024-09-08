f9178ba757cdb92c0e8d09f334e1024758a13a19
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-09-08 17:28:32 +0300

    Created Base instance

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
@@ -6,3 +7,4 @@ engine = create_async_engine(settings.database_url, echo=True)
 
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
+Base = declarative_base()
