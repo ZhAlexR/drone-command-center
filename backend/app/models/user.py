4bff45d5aa690cfee85968331ec4037518b25cad
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-09-10 23:08:48 +0300

    Updated colum initialization

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -7,7 +7,7 @@ class Role(BaseModel):
     __tablename__ = 'roles'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: str = mapped_column(String(30), unique=True)
+    name: Mapped[str] = mapped_column(String(30), unique=True)
 
     users: Mapped[list["User"]] = relationship(back_populates="role")
 
@@ -19,7 +19,7 @@ class User(BaseModel):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(30), unique=True, index=True)
-    email: str = mapped_column(unique=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str]
     is_active: Mapped[bool] = mapped_column(default=True)
     role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'))
