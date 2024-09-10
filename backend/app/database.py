28bf573d4104b418bf9c7ea5df1a90c7e9d6ffc6
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-09-10 21:16:30 +0300

    Changed BaseModel declaration way

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from config import settings
 
 engine = create_async_engine(settings.database_url, echo=True)
 
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-Base = declarative_base()
+class BaseModel(DeclarativeBase):
+    pass
