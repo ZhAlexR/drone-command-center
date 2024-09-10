ffbd22f448f78e69e825031edf23bf3dd6b0368b
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-09-10 22:41:44 +0300

    Made adjustments for async migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,20 +7,28 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from backend.app.config import settings
+from backend.app.models.base import BaseModel
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+
+
+config.set_main_option("sqlalchemy.url", settings.database_url)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -66,7 +74,9 @@ async def run_async_migrations() -> None:
     """
 
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {
+            'sqlalchemy.url': settings.database_url  # Use the dynamic database URL
+        },
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
