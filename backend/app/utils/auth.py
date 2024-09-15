0f492ad99cfc3c2a82156fe4a0b4f597463b54eb
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-09-15 20:01:26 +0300

    Added user authentication utils

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -4,19 +4,20 @@ import jwt
 
 from passlib.context import CryptContext
 from backend.app.config import settings
+from backend.app.shemas.token_schema import Token
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_access_token(data: dict):
     data_to_encode = data.copy()
-    expire = datetime.now(UTC) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(UTC) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     data_to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         payload=data_to_encode,
-        key=settings.SECRET_KEY,
+        key=settings.JWT_SECRET_KEY,
         algorithm=settings.JWT_ALGORITHM,
     )
-    return encoded_jwt
+    return Token(token=encoded_jwt)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
