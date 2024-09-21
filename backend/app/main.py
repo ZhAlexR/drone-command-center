7a6e039d14e89b4a73d8a5cdda2e7e86ca53a975
ZhAlexR <oleksandrzhukov95@gmail.com>
2024-09-21 12:30:13 +0300

    Added protected route for test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+
+from backend.app.dependency import RoleChecker
 from backend.app.routes.auth import auth_router
 
 app = FastAPI()
@@ -8,3 +12,8 @@ app.include_router(router=auth_router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Drone Command Center!"}
+
+
+@app.get("/protected_route")
+async def protected_route(_: Annotated[bool, Depends(RoleChecker(allowed_roles=["Operator"]))]) -> dict:
+    return {"message": "here is the protected route"}
